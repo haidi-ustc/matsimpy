2fbc0bc92cb4db111dd8b5aae5dca3354921993d
haidi <haidi@hfut.edu.cn>
2025-11-05 09:29:29 +0800

    Move test_quick_fixes.py to tests folder

--- tests/test_quick_fixes.py
+++ tests/test_quick_fixes.py
@@ -1,7 +1,7 @@
 """Quick test to verify all fixes work."""
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import numpy as np
 from matsimpy.core import Crystal, Lattice, Molecule
